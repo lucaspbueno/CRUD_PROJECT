a61d47ef59442968a3a4aab3825f0777fc8ce61a
lucaspbueno <lucaspbueno22@gmail.com>
2024-09-04 21:44:58 -0300

    feat: adiciona um requirements.txt e ajusta a configuracao do docker e do vite para habilitar a comunicacao entre as aplicacoes

--- back_end/api_root/settings.py
+++ back_end/api_root/settings.py
@@ -13,7 +13,13 @@ SECRET_KEY = 'django-insecure-(#kaa^8ucse_x35%vpb*=*#528&%n@e#z8ek#&!ykp(febuxxf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'frontend',
+    'front_end',
+    '0.0.0.0',
+]
 
 
 # Application definition
@@ -26,12 +32,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api_rest'
+    "corsheaders",
+    'api_rest',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -111,3 +119,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS_ALLOW_ALL_ORIGINS: True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://front_end:5173",
+    "http://frontend:5173",
+    "http://localhost:5173",
+    "http://0.0.0.0:5173",
+]
