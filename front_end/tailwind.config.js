5fb3ebbc9bdc98dc0a690c24579310a37224ebf7
lucaspbueno <lucaspbueno22@gmail.com>
2024-09-05 22:10:26 -0300

    fix: adiciona os arquivos que estavam send excluidos pelo gitignore

---
+++ front_end/tailwind.config.js
@@ -0,0 +1,25 @@
+import daisyui  from 'daisyui'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: ["class"],
+    content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
+  },
+  plugins: [
+    daisyui,
+      require("tailwindcss-animate")
+],
+}
+
