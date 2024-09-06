14b069a94508deb8a8d56f541c49c22e25124342
lucaspbueno <lucaspbueno22@gmail.com>
2024-09-06 01:31:39 -0300

    fix: remove o arquivo entrypoin.backend.sh e passa suas atribuicoes para o dockerfile do backend

--- entrypoint.backend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# entrypoint.sh
-
-# Executa migrações
-python manage.py makemigrations
-python manage.py migrate
-
-# Inicia o servidor Django
-exec python manage.py runserver 0.0.0.0:8000
